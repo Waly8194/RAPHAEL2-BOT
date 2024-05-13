dff2cb034fcc24919fae4701ef8110a97830a71b
Robinbot8194 <Robin.bot.8194@gmail.com>
2024-05-13 03:03:08 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -19,8 +19,8 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    await conn.sendMessage(m.chat, { react: { text: '🩸', key: m.key } })
-let videoUrl = 'https://telegra.ph/file/a1e17b9b65eef57e8ee82.mp4';
+    await conn.sendMessage(m.chat, { react: { text: '🦋', key: m.key } })
+let videoUrl = 'https://telegra.ph/file/5cf769878fc899f9324f5.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
@@ -36,15 +36,15 @@ let more = String.fromCharCode(8206)
 let readMore = more.repeat(900) 
   const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
 let str = ` 
-*⧠━──━⧈⇓《🩸》⇓⧈━──━⧠*
-*❖┃اهلا بك ف بوت ايتاشي*
+*┃❐━═━═✦◈❨🦋❩◈✦═━═━❐┃*
+*❖┃اهلا بك ف بوت روبين*
 
 *❐↞┇اذا كان امر لا يعمل ابلغ المطور عن طريق امر .بلاغ*
 *❂ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*
-*╟❧ لعرض الاوامر ↞ .اوامر*
-*╟❧ لعرض المهام ↞ .المهام*
+*┃  لعرض الاوامر ↞ .اوامر ➪*
+*┃  لعرض المهام ↞ .المهام ➪*
 *❂ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*
-*⧠━──━⧈⇓《🩸》⇓⧈━──━⧠*
+*┃❐━═━═✦◈❨🦋❩◈✦═━═━❐┃*
 ‬
 ‬`.trim();
 
@@ -56,7 +56,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['بوت'] 
+handler.command = ['روبين'] 
 
 export default handler
 function clockString(ms) {
