0f22f4e21b50c1071259b55aa024a354a9074991
Robinbot8194 <Robin.bot.8194@gmail.com>
2024-05-14 00:28:41 +0300

    Create 2config.js

---
+++ plugins/2config.js
@@ -0,0 +1,45 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['201276848689', '201279800415', true],
+] //Number of owners
+
+global.mods = ['201279800415','201276848689'] 
+global.prems = ['201279800415','201276848689']
+
+
+global.APIs = { // API Prefix
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+
+global.APIKeys = { // Apikey : 42x5rO7o \\
+  'https://api-fgmods.ddns.net': '42x5rO7o'
+}
+
+// Sticker WM
+global.ownername = 'Deku'
+global.botname = 'روبين بــوت'
+global.premium = 'false'
+global.packname = 'روبين بــوت'
+global.author = '201279800415 >> ديكو 
+
+
+global.wait = '*[■■■■■■■■■□] 90%*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌'
+global.xmoji = '🔥'
+
+
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias //
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
