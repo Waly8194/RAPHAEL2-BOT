d022bbb1022d79af258183395594499f69cfa101
Robinbot8194 <Robin.bot.8194@gmail.com>
2024-05-13 14:45:03 +0300

    Update سكس 6.js

--- plugins/سكس 6.js
+++ plugins/سكس 6.js
@@ -17,7 +17,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
       name = conn.getName(who);
     }
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://api.waifu.pics/sfw/siks);
+    let apislap = await fetch(`https://api.waifu.pics/sfw/xnxxn);
     let jkis = await apislap.json();
     let { url } = jkis;
     let stiker = await sticker(null, url, `${name2} n\Slap `, null);
