670fffc9e581339d40c6d5427b66b0c1869d14f1
Robinbot8194 <Robin.bot.8194@gmail.com>
2024-05-22 21:06:45 +0300

    Update الدعم.js

--- plugins/الدعم.js
+++ plugins/الدعم.js
@@ -20,7 +20,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     await conn.sendMessage(m.chat, { react: { text: '🦾', key: m.key } })
-let videoUrl = 'https://telegra.ph/file/effbcf1d588efd0d1a51e.mp4';
+let videoUrl = 'https://telegra.ph/file/26b8ba17d459e0a32321f.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
